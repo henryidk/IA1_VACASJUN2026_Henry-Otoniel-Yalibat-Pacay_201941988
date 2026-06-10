11ae722a59968bc7c89ceda865c6e740e1e930a5
henryidk <henryalibat4@gmail.com>
2026-06-10 09:36:56 -0600

    cors cambios en docker

--- Dr_Byte_FASE1/backend/app.py
+++ Dr_Byte_FASE1/backend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from dotenv import load_dotenv
 import os
 
@@ -6,6 +7,7 @@ load_dotenv()
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
 
     from routes.diagnostico import diagnostico_bp
     from routes.historial import historial_bp
